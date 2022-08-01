5db694d7d1b85474e5ff890c7fce066793ba015d
RJHARENBERG <reinardharenberg@gmail.com>
2022-08-01 17:06:41 +0200

    react werkt

---
+++ yelloowCustomVisualeDemo/.tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -0,0 +1,4 @@
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+declare var yelloowCustomVisualeDemo1058965490D845AA97D264292AC47891_DEBUG: IVisualPlugin;
+export default yelloowCustomVisualeDemo1058965490D845AA97D264292AC47891_DEBUG;
